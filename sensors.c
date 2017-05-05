14abd23b5672d23dca13b98b3ddff82bab073f9c
Mat Ruane <matr800@gmail.com>
2017-05-05 17:33:38 +1200

    added a take average method that takes an average reading of a given IR sensor.

--- sensors.c
+++ sensors.c
@@ -5,6 +5,7 @@
 /*
 	- assumes that the main class initialises the RPi.
 	- will need to rename sensor pin globals if we use multiple sensors of one type.
+		- likely other things will need to change too.
 */
 
 /* Global variables */
@@ -38,3 +39,29 @@ Boolean readPushButton() {
 
 /* Average readings */
 
+double takeAverage(String sensor, int duration = 100, int samples = 20) {
+	// Returns the average of _samples_ samples over _duration_ milliseconds
+	// for given IR sensor type.
+	// TODO: make this work for durations greater than 1000ms (1 second).
+	double sum = 0;
+	int delayBetweenSamples = duration / samples;
+
+	for (int i = 0; i < samples; i++) {
+		double value = 0;
+
+		switch(sensor) {
+			case 'ShortRangeIR' :
+				value = readShortRangeIR();
+			case 'MedRangeIR' :
+				value = readMedRangeIR();
+			default :
+				printf("Invalid sensor name given to takeAverage\n");
+				break;
+		}
+
+		sum += value;
+		sleep1(0, delayBetweenSamples * 1000); // sleep1 takes microseconds
+	}
+
+	return sum / samples;
+}
