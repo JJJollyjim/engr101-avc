b90baa47dfcec533e0b298c28b9a5dd30d66f35e
Mat Ruane <matr800@gmail.com>
2017-05-17 11:56:30 +1200

    A first attempt at implementing derivative component

--- robot/src/robot.cpp
+++ robot/src/robot.cpp
@@ -12,6 +12,7 @@ String GATE_PORT = 0;
 
 //Tuning
 float KP = 0.5; // proportionality constant
+float KD = 0.5; // derivative constant
 int BASE_SPEED = 50;
 
 int openGate() {
@@ -21,15 +22,29 @@ int openGate() {
 
 int drive() {
 
+    int error;
+    int previous_error = 0;
     int proportional_signal;
+    float prop_component;
+    int derivative_signal;
+    float deriv_component;
+    float PID_sum;
     int deltaLeft;
     int deltaRight;
 
     while(true) {
-        proportional_signal = horizontalSample() * KP;
+        error = horizontalSample(); // Value between -120 and 120
+        proportional_signal = error * KP;
+        derivative_signal = (error - previous_error) * KD; // error - previous: range -240 to 240
+        previous_error = error;
 
-        deltaLeft = proportional_signal/(120*1*KP)*254; // 254 because of bug in library...
-        deltaRight = -1*proportional_signal/(120*1*KP)*254 ; //... Motors can run endlessly if set to 255.
+        prop_component = proportional_signal/(120*1.0*KP);
+        deriv_component = derivative_signal/(240*1.0*KD); //?
+
+        PID_sum = prop_component + deriv_component;
+
+        deltaLeft = PID_sum*254; // 254 because of bug in library...
+        deltaRight = -1*PID_sum*254 ; //... Motors can run endlessly if set to 255.
 
         set_motor(1, (BASE_SPEED + deltaLeft));
         set_motor(2, (BASE_SPEED + deltaRight));
@@ -48,22 +63,21 @@ int drive() {
         **/
 
         //TODO: sleep1 seems to cause compilation errors (undefined reference) when called within a while loop
-        // sleep1(0,200000); // 0.2 seconds
+        // atleast when compiling on a RPi 3, our robot may be different
+        sleep1(0,100000); // 0.1 seconds - 10FPS
     }
     return 0;
 }
 
-
 int main() {
     init();
-    
-    
-    while(true) {
-		printf("Current Error: %d", horizontalSample());		
-		display_picture(3,0);		
-		}
 
-    //drive();
+    drive();
+
+    //TODO: Implement a way to break out of drive loop and safely stop motors - Wait for an input in terminal?
+
+    // Found within e101 lib - unclear how it works, e101 wiki and Arthur's notes were no help.
+    // stop();
 
     return 0;
 }
